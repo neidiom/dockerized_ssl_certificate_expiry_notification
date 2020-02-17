93a1f44db3359b2db5d43047a68eb788398c5502
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2020-02-17 11:44:55 +0800

    adding .env to .gitignore and updating the buils script to test is vars.yml and .env files exist

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
+#!/usr/bin/env bash
+VARS_FILE=app/vars.yml
+ENV_FILE=.env
+
+if test -f "$VARS_FILE"; then
+    echo "$VARS_FILE file exist, doing nothing."
+else
+    echo "$VARS_FILE does not exist, creating $VARS_FILE file."
+    cp app/vars.yml.example $VARS_FILE
+fi
+
+if test -f "$ENV_FILE"; then
+    echo "$ENV_FILE file exist, doing nothing."
+else
+    echo "$ENV_FILE does not exist, creating $ENV_FILE file."
+    cp env.example $ENV_FILE
+fi
+
 sudo docker-compose build
 sudo docker-compose up --force-recreate -d ssl_domain_expiry_check
